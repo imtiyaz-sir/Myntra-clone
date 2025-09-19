62d3013d065bd5e7cd894a331ad1d243c858b4da
imtiyaz-sir <allrounder8787@gmail.com>
2025-09-19 22:45:22 +0530

    Update items.js

--- data/items.js
+++ data/items.js
@@ -32,7 +32,7 @@ const items = [
     },
     {
         id: '003',
-        image: '/images/3.jpg',
+        image: 'images/3.jpg',
         company: 'NUEVOSDAMAS',
         item_name: 'Women Red & White Printed A-Line Knee-Length Skirts',
         original_price: 1599,
@@ -47,7 +47,7 @@ const items = [
     },
     {
         id: '004',
-        image: '/images/4.jpg',
+        image: 'images/4.jpg',
         company: 'ADIDAS',
         item_name: 'Indian Cricket ODI Jersey',
         original_price: 999,
@@ -62,7 +62,7 @@ const items = [
     },
     {
         id: '005',
-        image: '/images/5.jpg',
+        image: 'images/5.jpg',
         company: 'Roadster',
         item_name: 'Pure Cotton T-shirt',
         original_price: 1399,
@@ -77,7 +77,7 @@ const items = [
     },
     {
         id: '006',
-        image: '../images/6.jpg',
+        image: 'images/6.jpg',
         company: 'Nike',
         item_name: 'Men ReactX Running Shoes',
         original_price: 14995,
@@ -92,7 +92,7 @@ const items = [
     },
     {
         id: '007',
-        image: '../images/7.jpg',
+        image: 'images/7.jpg',
         company: 'The Indian Garage Co',
         item_name: 'Men Slim Fit Regular Shorts',
         original_price: 1599,
@@ -105,7 +105,7 @@ const items = [
     },
     {
         id: '008',
-        image: '../images/8.jpg',
+        image: 'images/8.jpg',
         company: 'Nivea',
         item_name: 'Men Fresh Deodrant 150ml',
         original_price: 285,
@@ -120,7 +120,7 @@ const items = [
     },
     {
         id: '009',
-        image: '/images/9.jpg',
+        image: 'images/9.jpg',
         company: 'IMAX',
         item_name: 'full dress',
         original_price: 485,
@@ -135,7 +135,7 @@ const items = [
     },
     {
         id: '10',
-        image: '/images/10.jpg',
+        image: 'images/10.jpg',
         company: 'pogo',
         item_name: 'long full saree',
         original_price: 785,
@@ -150,7 +150,7 @@ const items = [
     },
     {
     id: '11',
-    image: '/images/11.jpg',
+    image: 'images/11.jpg',
     company: 'CN',
     item_name: 'new style pant shirt',
     original_price: 885,
@@ -165,7 +165,7 @@ const items = [
 },
 {
     id: '12',
-    image: '/images/12.jpg',
+    image: 'images/12.jpg',
     company: 'Moon',
     item_name: 'flexble mobile stand ',
     original_price: 486,
